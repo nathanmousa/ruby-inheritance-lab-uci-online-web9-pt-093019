ecab4f485265cfa1851542d17c541b7010325fd0
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-09 16:09:37 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+class User
+  attr_accessor :first_name, :last_name, :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string_of_knowledge)
+    knowledge << string_of_knowledge
+  end
+end
